afed84624d651842dd9bc79547b16713e9a120fc
Cody J Champion <cchampio@nmsu.edu>
2017-11-19 14:34:39 -0700

    Add files via upload

---
+++ AsymmMultilaneCP.m
@@ -0,0 +1,120 @@
+clear all
+
+
+    clearvars -except density
+%Starting variables
+density=.9;
+numlanes=2;
+%numcars=249;
+gridpts=250;
+numcars=round(density*gridpts*numlanes);
+grid=zeros(numlanes,gridpts);
+vmax=10;
+badDriverp=0;
+passfactor=1;
+
+%Characteristics of Each car
+%Column 1=starting position
+%Column 2=ending position
+%Column 3=current velocity
+%Column 4=Current Lane
+%Column 5=End Lane
+%Column 6=Car type
+%Column 7=Rank of vehicle
+%Column 8=Rudeness Factor
+%Column 9=Personalized Max Speed
+
+carChar=zeros(numcars,9);
+carChar(:,3)=round(vmax*rand(1,numcars)); %Allowed to have same velocity though
+
+vehdist=zeros(1,100);
+vehdist(1:95)=1;
+vehdist(96:100)=2;
+carChar(:,6)=randsample(vehdist,numcars,true);  %replacement is being allowed
+
+carChar(:,8)=rand(1,numcars);
+
+%setting up grid
+for ii=1:numcars
+    carChar(ii,4)=mod(ii,numlanes)+1;
+    carChar(ii,1)=mod(ii,gridpts)+1;
+   grid(carChar(ii,4),carChar(ii,1))=carChar(ii,6);
+    
+end
+
+carChar(:,9)=round(10*betarnd(5,5,[1,numcars]));
+%%
+%Set new characteristics
+counter=0;
+while min(carChar(:,1))<gridpts && counter<gridpts*2
+    [a,carChar(:,7)]=sort(carChar(:,1),'descend');
+    %accident
+%     if counter>30 && counter <60
+%        grid(:,gridpts/2)=1000; 
+%     end
+        
+    for ii=1:numcars
+        [a,loc]=min(carChar(:,7));
+        clear gap*
+        if carChar(loc,1)>=gridpts
+            carChar(loc,:)=inf;
+        end
+          if carChar(loc,1)~=inf
+              vmax=carChar(loc,9);
+              %%Lane Changes
+              %Moving to rightmost available lane
+              if carChar(loc,4)~=1 && sum(sum(grid(:,max([1,carChar(loc,1)]):min([carChar(loc,1)+vmax,gridpts])))) && passfactor>=carChar(loc,8)  %if not in rightmost land and no one within hitting distance in front of you
+                  if sum(grid(1:max([1,carChar(loc,4)-1]),carChar(loc,1)))==0  %If no one in any of the lanes to the right of them in their same position
+                      carChar(loc,5)=1;
+                  else
+                      gapsr=carChar(loc,4)-find(grid(:,4)~=0);
+                      gapsr(gapsr>=0)=-inf;
+                      gapr=-1*max(gapsr)-1;
+                      carChar(loc,5)=max([1,carChar(loc,4)-gapr]);
+                  end
+              %Or moving left one lane
+              elseif carChar(loc,4)~=numlanes && sum(sum(grid(:,carChar(loc,1):min([gridpts,carChar(loc,1)+vmax]))))~=0 && sum(grid(carChar(loc,4)+1,max([1,carChar(loc,1)-vmax]):carChar(loc,1)))==0 &&passfactor>carChar(loc,8)
+                 carChar(loc,5)=carChar(loc,4)+1; 
+              else
+                  carChar(loc,5)=carChar(loc,4);
+              end
+          %Forward Movement
+              gapsf=carChar(loc,1)-find(carChar(:,5)==carChar(loc,4));
+              gapsf(gapsf>=0)=-inf;
+              gapf=-1*(max(gapsf))-1;    %number of empty sites ahead
+              if size(gapf,1)~=0
+                  gapveh=find(carChar(:,1)==carChar(loc,1)+gapf+1);
+                  if carChar(gapveh(:),6)==2
+                      gapf=max([gapf-1,0]);
+                  end
+              else 
+                  gapf=inf;
+              end
+              
+              carChar(loc,3)=max([0,min([gapf,vmax,carChar(loc,3)+1])]);
+              carChar(loc,3)=max([carChar(loc,3)-binornd(1,badDriverp*carChar(loc,8)),1]);
+              carChar(loc,2)=carChar(loc,3)+carChar(loc,1);            
+              
+          %Update Grid
+          grid(carChar(loc,4),carChar(loc,1))=0;   %Their old grid point is now empty
+          grid(carChar(loc,5),carChar(loc,2))=carChar(loc,6);    %Their new grid point is now its car type
+          end
+          carChar(loc,7)=inf;
+    end
+    carChar(:,1)=carChar(:,2);
+    carChar(:,4)=carChar(:,5);
+    counter=counter+1;
+
+    figure(3)
+    plot(counter,carChar(:,2),'Marker','*')
+    %plot(counter,carChar(:,4),'Marker','*');
+    hold on
+   vehcs=find(carChar(:,1)~=inf);
+    avgv(counter)=mean(carChar(vehcs(:),3));
+    %dact(counter)=size(vehcs,2)/gridpts/numlanes;
+    Q(counter)=density*avgv(counter);
+end
+ 
+
+carChar
+counter
---
+++ TwoLanecomp6Dens2.m
@@ -0,0 +1,160 @@
+clear all
+for density=0.01:0.005:1
+    clearvars -except density
+%Starting variables
+numlanes=2;
+%numcars=249;
+gridpts=250;
+numcars=ceil(density*gridpts*numlanes);
+grid=zeros(numlanes,gridpts);
+vmax=5;
+badDriverp=0.3;
+passfactor=0.75;
+
+%Characteristics of Each car
+%Column 1=starting position
+%Column 2=ending position
+%Column 3=current velocity
+%Column 4=Current Lane
+%Column 5=End Lane
+%Column 6=Car type
+%Column 7=Direction of lane change
+%Column 8=Passing Probability
+%Column 9=Personalized Max Speed
+
+carChar=zeros(numcars,9);
+carChar(1:ceil(numcars/2),1)=randsample(gridpts+1,ceil(numcars/2));
+carChar(ceil(numcars/2)+1:numcars,1)=randsample(gridpts+1,numcars-ceil(numcars/2));
+%carChar(:,1)=randsample(gridpts,numcars);    %randsample because not allowed to start in same place
+carChar(:,3)=round(vmax*rand(1,numcars)); %Allowed to have same velocity though
+carChar(1:ceil(numcars/2),4)=1;
+carChar(ceil(numcars/2)+1:numcars,4)=2;
+
+vehdist=zeros(1,100);
+vehdist(1:95)=1;
+vehdist(96:100)=2;
+carChar(:,6)=randsample(vehdist,numcars,true);  %replacement is being allowed
+
+carChar(:,8)=rand(1,numcars);
+
+counter=0;
+
+%setting up grid
+for ii=1:numcars
+   grid(carChar(ii,4),carChar(ii,1))=carChar(ii,6);
+    
+end
+
+carChar(:,9)=round(10*betarnd(8,2,[1,numcars]));
+
+%%
+%Set new characteristics
+while min(carChar(:,1))<gridpts && counter<gridpts*2
+    carChar(:,7)=0;
+   
+    %accident
+%     if counter>30 && counter <60
+%        grid(:,gridpts/2)=4; 
+%     end
+        
+    for ii=1:numcars
+        clear gap*
+        if carChar(ii,1)>=gridpts
+            carChar(ii,:)=inf;
+          end
+          if carChar(ii,1)~=inf
+              vmax=carChar(ii,9);
+        %People in the right lane
+        if carChar(ii,4)==1
+            gapsf=carChar(ii,1)-find(grid(1,:)~=0);
+            gapsf(gapsf>=0)=-inf;
+            gapf=-1*(max(gapsf))-1;    %number of empty sites ahead
+            gapveh=find(carChar(:,1)==carChar(ii,1)+gapf+1);
+            if carChar(gapveh(:),6)==2
+                gapf=gapf-1;
+            end
+                
+            if carChar(ii,3)>gapf && grid(2,carChar(ii,1))==0 && sum(grid(2,max([carChar(ii,1)-vmax,1]):carChar(ii,1)))==0 &&passfactor>carChar(ii,8)   %If could hit someone in front and there's no one besid and no one would hit you if switched lanes,
+                carChar(ii,7)=1; %Car should move into left lane
+                if sum(grid(2,carChar(ii,1):min([carChar(ii,1)+vmax,gridpts])))~=0
+                    gapsfo=carChar(ii,1)-find(grid(2,:)~=0);
+                    gapsfo(gapsfo>=0)=-inf;
+                    gapfo=-1*(max(gapsfo))-1;
+                    gapveh=find(carChar(:,1)==carChar(ii,1)+gapfo+1);
+                    if carChar(gapveh(:),6)==2
+                        gapfo=max([gapfo-1,0]);
+                    end
+                    carChar(ii,3)=min([gapfo,vmax,carChar(ii,3)+1]);
+                        
+                else 
+                    carChar(ii,3)=min([vmax,carChar(ii,3)+1]);
+                end
+                 
+            else %If all weren't met
+                    carChar(ii,3)=min([gapf,vmax,carChar(ii,3)+1]);
+            end
+            
+        %People in the second (left) lane
+        elseif carChar(ii,4)==2
+            if sum(grid(1,min([gridpts,max([carChar(ii,1)-vmax,1])]):carChar(ii,1)))==0 && sum(grid(1,max([carChar(ii,1),1]):min([gridpts,carChar(ii,1)+min([vmax,carChar(ii,3)+1])])))==0 && passfactor>carChar(ii,8)
+                carChar(ii,3)=min([vmax,carChar(ii,3)+1]);
+                carChar(ii,7)=-1;                
+            else
+                gapsf=carChar(ii,1)-find(grid(2,:)~=0);
+                gapsf(gapsf>=0)=-inf;
+                gapf=-1*(max(gapsf))-1;
+                gapveh=find(carChar(:,1)==carChar(ii,1)+gapf+1);
+            if carChar(gapveh(:),6)==2
+                gapf=max([gapf-1,0]);
+            end
+                carChar(ii,3)=min([gapf,vmax,carChar(ii,3)+1]);
+            end
+        end
+            
+          end
+    end
+    
+            carChar(:,5)=carChar(:,4)+carChar(:,7);
+            carChar(:,2)=carChar(:,1)+carChar(:,3);
+            carChar(:,1)=carChar(:,2);
+    grid=zeros(numlanes,gridpts);        
+    for ii=1:numcars
+        if carChar(ii,1)~=inf
+            carChar(ii,3)=max([carChar(ii,3)-binornd(1,badDriverp),0]);
+            grid(carChar(ii,5),max([1,carChar(ii,2)]))=carChar(ii,6);    %update grid
+        end
+    end
+    carChar(:,4)=carChar(:,5);
+
+    %plot(counter,carChar(:,2),'Marker','*')
+    %plot(counter,carChar(:,4),'Marker','*');
+    %hold on
+
+    counter=counter+1;
+   vehcs=find(carChar(:,1)~=inf);
+    avgv(counter)=mean(carChar(vehcs(:),3));
+    %dact(counter)=size(vehcs,2)/gridpts/numlanes;
+    Q(counter)=density*avgv(counter);
+
+end
+    figure(1)
+ plot(density,mean(Q),'Marker','*')
+ hold on
+ figure(2)
+ plot(density,mean(avgv),'Marker','*')
+ hold on
+end
+figure(1)
+xlabel('Initial Density')
+ylabel('Average Flow')
+title('Fundamental Diagram with Even Spread Medium Speed Beta Distribution')
+hold off
+
+figure(2)
+xlabel('Initial Density')
+ylabel('Average Velocity')
+title('Average Velocity with Even Spread Medium Beta Distribution vs. Initial Density')
+hold off
+
+carChar
+counter
